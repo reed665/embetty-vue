92f2dfb8cd86d6bb7d6e9572dcb3eebe484ae225
Aleksandr Buiko <aleksandr.buiko@t-systems.com>
2022-08-23 15:44:52 +0300

    drop videos except youtube

--- src/components/video-impl/index.js
+++ src/components/video-impl/index.js
@@ -1,11 +1,5 @@
-import FacebookVideo from './FacebookVideo';
-import NativeVideo from './NativeVideo';
-import VimeoVideo from './VimeoVideo';
 import YoutubeVideo from './YoutubeVideo';
 
 export var videoImplementations = {
-  facebook: FacebookVideo,
-  native: NativeVideo,
-  vimeo: VimeoVideo,
   youtube: YoutubeVideo
 };
