b60a4a27d6cbb4ff32522806acc2531432698ae5
Aleksandr Buiko <aleksandr.buiko@t-systems.com>
2022-08-25 09:47:39 +0300

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@antfu',
+  ],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+  },
+  ignorePatterns: [
+    'index.html',
+    'package.json',
+  ],
+}
